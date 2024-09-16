201b2429acd7a529aabeef361aec95e8623f4e46
Ismael <ismael.lajud65@gmail.com>
2024-09-16 20:55:01 +0200

    actualizando

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,13 +1,32 @@
 import '../Styles/FooterStyles.css'
+import SvgIconGIT from './SvgIconGIT'
+import SvgIconInstagram from './SvgIconInstagram'
+import SVGIconLinkeding from './SVGIconLinkeding'
 
 const Footer = () => {
   return (
    <footer className='footer-container'>
-    <ul>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
+    <div className='info-container'>
+
+   
+    <ul className='footer-ul'>
+        <li className='li-container'>CHARACTERS:</li>
+        <li className='li-container'>LOCATIONS:</li>
+        <li className='li-container'>EPISODES:</li>
     </ul>
+    <span className='server-text'>Server Status <span className='server-icon'></span></span>
+    </div>
+    <div className='social-mediacontainer'></div>
+      <ul className='social-medialist'>
+        <li className='social-media-icon'><SvgIconInstagram/></li>
+        <li className='social-media-icon'><SVGIconLinkeding/></li>
+        <li className='social-media-icon'><SvgIconGIT/></li>
+      </ul>
+      <div/>
+          
+    <div>
+      <span>by Ismael Lajud </span>
+    </div>
    </footer>
   )
 }
