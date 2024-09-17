30d70e9de037c9048c0894253f68048316f9bf08
Ismael <ismael.lajud65@gmail.com>
2024-09-17 14:03:01 +0200

    actualizando

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,7 +2,7 @@ import '../Styles/HeroStyles.css'
 const Hero = () => {
   return (
     <section className='hero-section'>
-            <h1 className='hero-title'>RICK AND MORTY🦠</h1>
+            <h1 className='hero-title'>RICK AND MORTY</h1>
     </section>
   )
 }
